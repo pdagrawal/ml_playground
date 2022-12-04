5bd5bfbf272c076cb3ebb818cc19855ff56ab09c
Pradeep Agrawal <p98@umbc.edu>
2022-12-04 15:20:46 -0500

    Added svm

--- ml_playground/apps/public/urls.py
+++ ml_playground/apps/public/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("about", views.about, name="about"),
     path("upload_csv", views.upload_csv, name="upload_csv"),
     path("work_in_progress", views.work_in_progress, name="work_in_progress"),
+    path("train_model", views.train_model, name="train_model"),
+    path("prediction_result", views.prediction_result, name="prediction_result"),
 ]
--- ml_playground/apps/public/views.py
+++ ml_playground/apps/public/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from django.contrib import messages
 
+import pandas as pd
+import pickle
+# import numpy as np
+# import seaborn as sb
+# import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.svm import SVC
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import confusion_matrix
+# from sklearn.model_selection import train_test_split
+
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
@@ -65,3 +76,70 @@ def upload_csv(request: HttpRequest) -> HttpResponse:
 def work_in_progress(request: HttpRequest) -> HttpResponse:
     messages.success(request, f"Filepath: {request.session['uploaded_file_path']}")
     return render(request, "work_in_progress.html")
+
+def create_svm_model(training_samples, clf_labels, **svm_params):
+    svm_model = SVC(**svm_params)
+    svm_model.fit(training_samples, clf_labels)
+
+    with open("tmp/iris.pkl", 'wb') as f:
+        pickle.dump(svm_model, f)
+
+
+def transform_dataset(df):
+    le = preprocessing.LabelEncoder()
+    for column_name in df.columns:
+        if df[column_name].dtype == object:
+            df[column_name] = le.fit_transform(df[column_name])
+    return df
+
+
+def process_dataset(dataset_file, training_params=[], clf_params=""):
+    df = pd.read_csv(dataset_file)
+    df.head()
+
+    # Transform non-numeric columns
+    df = transform_dataset(df)
+    # Get classification labels.
+    clf_labels = df[clf_params]
+    print(clf_labels.size)
+
+    # Drop the classification labels to form training parameters.
+    df.drop(clf_params, axis=1, inplace=True)
+
+    # Keep features which are selected by the user.
+    for param in list(set(df.columns.values) - set(training_params)):
+        df.drop(param, axis=1, inplace=True)
+
+    return [df, clf_labels]
+
+
+def test(model_file, sample):
+    with open(model_file, 'rb') as f:
+        model = pickle.load(f)
+    return model.predict(sample)
+
+def train_model(request: HttpRequest) -> HttpResponse:
+    print('=====================')
+    print(dict(request.POST.items()))
+    print('=====================')
+    print(dict(request.POST.getlist('attributes')))
+    request.POST.getlist('attributes')
+    # print(request.POST.get("classification"))
+    # print(request.POST.get("algorithm"))
+    print('=====================')
+    data = process_dataset("tmp/iris.csv", ["sepal_length", "sepal_width", "petal_length", "petal_width"], "class")
+    # data = process_dataset(request.session['uploaded_file_path'], ["sepal_length", "sepal_width", "petal_length", "petal_width"], "class")
+
+    svm_params = {'kernel': 'rbf'}
+    create_svm_model(data[0], data[1], **svm_params)
+
+    x_test = [[7.7, 2.6, 6.9, 2.3]]
+    sample = pd.DataFrame(
+        x_test, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+    sample = transform_dataset(sample)
+    predicted_value = test("tmp/iris.pkl", sample)
+    messages.success(request, "successfully predicted.")
+    return redirect('public:prediction_result', predicted_value = predicted_value)
+
+def prediction_result(request: HttpRequest, predicted_value) -> HttpResponse:
+    return render(request, "prediction_result.html", {'predicted_value': predicted_value})
