a955140f613fac701971011e218885ead5196801
sakshi6557 <reddy.sakshi97@gmail.com>
2022-12-04 20:54:23 -0500

    Other ML models

--- ml_playground/models/svm.py
+++ ml_playground/models/svm.py
@@ -5,6 +5,8 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.svm import SVC
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -17,6 +19,20 @@ def create_svm_model(training_samples, clf_labels, **svm_params):
 
     with open("tmp/iris.pkl", 'wb') as f:
         pickle.dump(svm_model, f)
+
+def create_multiple_regression(training_samples, clf_labels):
+    lin_reg=LinearRegression()  
+    lin_reg.fit(training_samples, clf_labels)
+
+    with open("tmp/multiple.pkl",'wb') as f:
+        pickle.dump(lin_reg,f)
+
+def create_logistic_regression(training_samples, clf_labels, **logistic_params):
+    logistic=LogisticRegression(**logistic_params)  
+    logistic.fit(training_samples, clf_labels)
+
+    with open("tmp/logistic.pkl",'wb') as f:
+        pickle.dump(logistic,f)
 
 def create_decisiontree_model(training_samples, clf_labels, **dt_params):
     dt_model = tree.DecisionTreeClassifier(**dt_params)
@@ -44,7 +60,7 @@ def process_dataset(dataset_file, training_params=[], clf_params=""):
     df.head()
 
     # Transform non-numeric columns
-    df = transform_dataset(df, clf_params)
+    df = transform_dataset(df, True, clf_params)
 
     # Get classification labels.
     clf_labels = df[clf_params]
@@ -66,30 +82,55 @@ def test(model_file, sample):
     with open('tmp/iris_encoder.pkl', 'rb') as handle:
         encoder = pickle.load(handle)
 
-    print(encoder.inverse_transform(model.predict(sample)))
+    print(encoder.inverse_transform([int(model.predict(sample))]))
+
 
 
 data = process_dataset("tmp/iris.csv",
                        ["sepal_length", "sepal_width", "petal_length", "petal_width"], "class")
-'''
+
 svm_params = {'kernel': 'rbf'}
 model = create_svm_model(data[0], data[1], **svm_params)
-
-#x_test = [[7.7, 2.6, 6.9, 2.3]]
-x_test = [[6.3, 2.3, 4.4, 1.3]]
-sample = pd.DataFrame(
-    x_test, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
-sample = transform_dataset(sample)
-test("tmp/iris.pkl", sample)
-'''
-
-dt_params = {}
-model = create_decisiontree_model(data[0], data[1], **dt_params)
 
 x_test = [[7.7, 2.6, 6.9, 2.3]]
 #x_test = [[6.3, 2.3, 4.4, 1.3]]
 sample = pd.DataFrame(
     x_test, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
 sample = transform_dataset(sample)
+test("tmp/iris.pkl", sample)
+
+
+dt_params = {}
+model = create_decisiontree_model(data[0], data[1], **dt_params)
+'''
+x_test = [[7.7, 2.6, 6.9, 2.3]]
+#x_test = [[6.3, 2.3, 4.4, 1.3]]
+sample = pd.DataFrame(
+    x_test, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+sample = transform_dataset(sample)
+'''
 test("tmp/iris_dt.pkl", sample)
 
+
+
+logistic_params = {'solver' : 'liblinear', 'random_state' : 0}
+model = create_logistic_regression(data[0], data[1], **logistic_params)
+'''
+x_test = [[7.7, 2.6, 6.9, 2.3]]
+#x_test = [[6.3, 2.3, 4.4, 1.3]]
+sample = pd.DataFrame(
+    x_test, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+sample = transform_dataset(sample)
+'''
+test("tmp/logistic.pkl", sample)
+
+
+mr_model = create_multiple_regression(data[0], data[1])
+'''
+x_test = [[7.7, 2.6, 6.9, 2.3]]
+#x_test = [[6.3, 2.3, 4.4, 1.3]]
+sample = pd.DataFrame(
+    x_test, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+sample = transform_dataset(sample)
+'''
+test("tmp/multiple.pkl", sample)
